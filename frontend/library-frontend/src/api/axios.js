95d7a12c616126632c6773597c8421cf6fbcfddc
divyarajpatidar-cmd <divyarajpatidar452@gmail.com>
2026-03-30 19:50:58 +0530

    backend link updated

--- frontend/library-frontend/src/api/axios.js
+++ frontend/library-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8085"
+  baseURL: "https://library-management-using-springboot.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
