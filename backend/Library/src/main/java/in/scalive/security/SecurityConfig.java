1e2b353f2b6bd7265b895eb961d3b996ac0b7f4a
divyarajpatidar-cmd <divyarajpatidar452@gmail.com>
2026-04-05 22:22:26 +0530

    Security origin updated

--- backend/Library/src/main/java/in/scalive/security/SecurityConfig.java
+++ backend/Library/src/main/java/in/scalive/security/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://*.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
